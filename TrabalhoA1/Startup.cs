8abd1b1d791e0e404ac4fd1b3d8166972b0e682e
alyne770 <limaalyne3@gmail.com>
2024-10-25 09:16:52 -0300

    Adicionar arquivos de projeto.

---
+++ TrabalhoA1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TrabalhoA1.Startup))]
+namespace TrabalhoA1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
